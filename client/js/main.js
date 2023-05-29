acb2c3e7efca309a09f11e846e5952f66657c5df
gucchunchun <yunataniguchi23@gmail.com>
2023-05-29 18:20:38 +0100

    css/ fixed question

--- client/js/main.js
+++ client/js/main.js
@@ -1,11 +1,17 @@
-let conditions = {};
-var generatedPlan;
-
 const onSubmit = async(event)=> {
     // defaultのsubmitイベントを停止
-    event.preventDefault();   
+    event.preventDefault();  
+    // インプット画面を隠し、AIのプラン表示画面を見えるようにする
+    userInput.classList.add('hidden');
+    submitButton.classList.add('hidden');
+    madePlan.classList.remove('hidden');
+    conditionsBox.classList.remove('hidden');
+    disappearFlag();
+    const loadingInterval = setInterval(toggleLoading,1000);
+
     const formData = new FormData(event.target);
     let prev
+    let conditions = {};
     // formdataはそのままではアクセス不可能なのでobject形式へ
     for(var pair of formData.entries()) {
         if(pair[0]==prev){       
@@ -21,30 +27,33 @@ const onSubmit = async(event)=> {
     let addUnit = tmp + "K japanese-yen";
     conditions.budget = addUnit;
 
-    generatedPlan = await generateResponse();
+    showConditons(conditions);
+
+    const generatedPlan = await generateResponse(conditions);
     console.log(generatedPlan);
-    showResponse(generatedPlan);
+
+    clearInterval(loadingInterval);
+    loadingMessage.innerHTML = "Your plan has been generated"
+    setTimeout(function(){
+        loadingMessage.classList.add("hidden");
+        showResponse(generatedPlan);
+    },2000);
 }
 
-const generateResponse = async()=>{
+const generateResponse = async(c)=>{
     // ローカルサーバーにユーザーのインプット（条件）をPOSTする
     const resGenerater = await fetch('http://localhost:3000/planner', {
         method: 'POST',
         headers: { 'Content-Type' : 'application/json'},
-        body: JSON.stringify(conditions)
+        body: JSON.stringify(c)
       });
     let data = await resGenerater.json();
     return data.response;    
 }
 
 function showResponse(response) {
-    userInput.classList.add('hidden');
-    submitButton.classList.add('hidden');
-    madePlan.classList.remove('hidden');
     const data = JSON.parse(response);
-    console.log(data);
     const days = Object.keys(data).length;
-    console.log(days);
     for(let i = 0; i < days; i++) {
         const section = document.createElement('section');
         // 装飾時の配置用
@@ -56,7 +65,6 @@ function showResponse(response) {
         dayTitle.innerHTML = dayNum;
         dayTitle.classList.add('dayNum');
         section.appendChild(dayTitle);
-        console.log(dayNum);
 
         // 場所ごとのプランが自動で振り分けられたindexにそれぞれ格納されている
         const plans = data[dayNum]; 
@@ -106,12 +114,14 @@ function showResponse(response) {
                         foodDiv.innerHTML += "Famous food: ";
                         for(let food of listContents) {
                             const foodSpan = document.createElement("span");
-                            foodSpan.innerHTML = food;
+                            foodSpan.innerHTML = food + ", ";
                             // flickerAPIとコネクトしてfoodの写真表示ができるようにする？
                             foodSpan.classList.add("foodName");
                             foodSpan.id = food;
                             foodDiv.appendChild(foodSpan);
                         }
+                        temp = foodDiv.lastChild.innerHTML;
+                        foodDiv.lastChild.innerHTML= temp.replace(", ","");
                         break;
                 }
             }
@@ -121,3 +131,87 @@ function showResponse(response) {
     }
 }
 
+function showConditons(conditions) {
+    console.log(conditions);
+    for(let key of Object.keys(conditions)) {
+        const p = document.createElement('p');
+        let array=conditions[key];
+        console.log(key);
+        switch(key) {
+            case "departPrefecture":
+                p.innerHTML = "Departure prefecture: " + array;
+                break;
+            case "destinationIsLike":
+                p.innerHTML = "Destination is like: ";
+                //[[[a,b]c]d]選択方法によって配列の形が異なるため
+                while(Array.isArray(array)){
+                    let newArray=[];
+                    for(let i=0; i<array.length; i++){
+                        if(Array.isArray(array[i])){
+                            newArray.push(array[i]);
+                        }else{
+                            p.innerHTML += array[i] + ", "
+                        }
+                    }
+                    array=newArray[0];
+                }
+                if(typeof array === "string"){
+                    p.innerHTML += array;
+                }
+                p.innerHTML=omitSpace(p.innerHTML);
+                break;
+            case "activity":
+                p.innerHTML = "Activities: ";
+                //[[[a,b]c]d]選択方法によって配列の形が異なるため
+                while(Array.isArray(array)){
+                    let newArray=[];
+                    for(let i=0; i<array.length; i++){
+                        if(Array.isArray(array[i])){
+                            newArray.push(array[i]);
+                        }else{
+                            p.innerHTML += " " + array[i] + ","
+                        }
+                    }
+                    array=newArray[0];
+                }
+                if(typeof array === "string"){
+                    p.innerHTML += array;
+                }
+                p.innerHTML=omitSpace(p.innerHTML);
+                break;
+            case "trip-duration":
+                p.innerHTML = "Duration: " + array;
+                break;
+            case "budget":
+                p.innerHTML = "Budget: " + array;
+                break;
+            case "travel-companions":
+                p.innerHTML = "Companions: " + array;
+                break;
+        }
+        conditionsBox.appendChild(p);
+    }
+}
+
+function disappearFlag() {
+    var sheets = document.styleSheets
+    var sheet = sheets[sheets.length - 1];
+
+    sheet.insertRule(
+        '#submitSpace::before,#submitSpace::after {display: none}',
+        sheet.cssRules.length
+      );
+}
+
+function toggleLoading() {
+    if(loadingMessage.style.display === 'none'){
+        loadingMessage.style.display = "block";
+    } else {
+        loadingMessage.style.display = "none";
+    }
+}
+
+function omitSpace(string) {
+    const spaceIndex = string.lastIndexOf(",");
+    return string.slice(0, spaceIndex);
+}
--- client/js/test.js
+++ client/js/test.js
@@ -1,5 +1,18 @@
-let conditions = {};
-
-conditions["country"] = "japan";
-
-console.log(conditions);
+body = "Destination is like: ";
+//[[[a,b]c]d]選択方法によって配列の形が異なるため
+var array="charming"
+while(Array.isArray(array)){
+    let newArray=[];
+    for(let i=0; i<array.length; i++){
+        if(Array.isArray(array[i])){
+            newArray.push(array[i]);
+        }else{
+            body += array[i] + " "
+        }
+    }
+    array=newArray[0];
+}
+if(typeof array === "string"){
+    body += array;
+}
+console.log(body);
